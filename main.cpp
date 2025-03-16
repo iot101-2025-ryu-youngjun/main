08f95345268b462586a62d9f022c5b7184df8d7c
류영준 <ggp09217@naver.com>
2025-03-16 22:35:28 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int RELAY=23;
+int SW=15;
+volatile bool relayOn=false;
+
+IRAM_ATTR void pushButton(){
+  relayOn= ! relayOn;
+}
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("Starting");
+  pinMode(RELAY,OUTPUT);
+  pinMode(SW,INPUT_PULLUP);
+  attachInterrupt( digitalPinToInterrupt(SW), pushButton,FALLING);
+  
+
+}
+
+void loop() {
+  delay(100);
+  if(relayOn){
+    digitalWrite(RELAY,HIGH);
+  }else{
+    digitalWrite(RELAY,LOW);
+  }
+}
+
